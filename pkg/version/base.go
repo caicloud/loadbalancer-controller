5fc2845aa3b6789dab678305b6bfa9834308775f
Jim Zhang <jim.zhang@caicloud.io>
2018-04-08 20:08:24 +0800

    feat(version): update pkg/version

---
+++ pkg/version/base.go
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2017 caicloud authors. All rights reserved.
+ */
+
+package version
+
+var (
+	// semantic version, derived by build scripts (see
+	// https://github.com/kubernetes/community/blob/master/contributors/design-proposals/release/versioning.md
+	// for a detailed discussion of this field)
+	//
+	// TODO: This field is still called "gitVersion" for legacy
+	// reasons. For prerelease versions, the build metadata on the
+	// semantic version is a git hash, but the version itself is no
+	// longer the direct output of "git describe", but a slight
+	// translation to be semver compliant.
+
+	// NOTE: The $Format strings are replaced during 'git archive' thanks to the
+	// companion .gitattributes file containing 'export-subst' in this same
+	// directory.  See also https://git-scm.com/docs/gitattributes
+	version      = "v0.0.0-master+$Format:%h$"
+	gitRemote    = ""            // git origin remote url
+	gitCommit    = "$Format:%H$" // sha1 from git, output of $(git rev-parse HEAD)
+	gitTreeState = ""            // state of git tree, either "clean" or "dirty"
+
+	buildDate = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
+)
