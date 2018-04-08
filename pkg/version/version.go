7d676202cb1a7e9fbea560445d796c71da39e0e3
Jun Zhang <jim.zhang@caicloud.io>
2018-04-08 20:20:04 +0800

    feat(version): update pkg/version (#50)

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -1,26 +1,53 @@
 /*
-Copyright 2017 Caicloud authors. All rights reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright 2017 caicloud authors. All rights reserved.
+ */
 
 package version
 
-var (
-	// RELEASE returns the release version
-	RELEASE = "UNKNOWN"
-	// REPO returns the git repository URL
-	REPO = "UNKNOWN"
-	// COMMIT returns the short sha from git
-	COMMIT = "UNKNOWN"
+import (
+	"encoding/json"
+	"fmt"
+	"runtime"
 )
+
+// Info contains versioning information.
+// TODO: Add []string of api versions supported? It's still unclear
+// how we'll want to distribute that information.
+type Info struct {
+	Version      string `json:"version"`
+	GitRemote    string `json:"gitRemote"`
+	GitCommit    string `json:"gitCommit"`
+	GitTreeState string `json:"gitTreeState"`
+	BuildDate    string `json:"buildDate"`
+	GoVersion    string `json:"goVersion"`
+	Compiler     string `json:"compiler"`
+	Platform     string `json:"platform"`
+}
+
+// Pretty returns a pretty output representation of Info
+func (info Info) Pretty() string {
+	str, _ := json.MarshalIndent(info, "", "    ")
+	return string(str)
+}
+
+func (info Info) String() string {
+	str, _ := json.Marshal(info)
+	return string(str)
+}
+
+// Get returns the overall codebase version. It's for detecting
+// what code a binary was built from.
+func Get() Info {
+	// These variables typically come from -ldflags settings and in
+	// their absence fallback to the settings in pkg/version/base.go
+	return Info{
+		Version:      version,
+		GitRemote:    gitRemote,
+		GitCommit:    gitCommit,
+		GitTreeState: gitTreeState,
+		BuildDate:    buildDate,
+		GoVersion:    runtime.Version(),
+		Compiler:     runtime.Compiler,
+		Platform:     fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+}
